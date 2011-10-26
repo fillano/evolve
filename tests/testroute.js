8e7269d5bfccd9e616af00f87d9f2d9db80f1dd1
fillano <fillano.feng@gmail.com>
2011-10-26 21:30:46 +0800

    start unit test

--- tests/testroute.js
+++ tests/testroute.js
@@ -1,4 +1,4 @@
-var route = require('../lib/router');
+/*var route = require('../lib/router');
 
 route.addhost('localhost');
 route.addfs('localhost', '/', '/usr/home');
@@ -6,3 +6,23 @@ route.addroute('localhost', '/upload', 'GET', function(){console.log('called');}
 console.dir(route.query('localhost', '/index.html', 'GET'));
 console.dir(route.query('localhost', '/upload', 'GET'));
 console.dir(route.query('localhost', '/upload', 'POST'));
+*/
+var testCase = require('nodeunit').testCase;
+
+module.exports = testCase({
+    setUp: function(cb) {
+        this.router = require('../lib/router');
+        cb();
+    },
+    tearDown: function(cb) {
+        this.router = null;
+        cb()
+    },
+    "testAddHost": function(test) {
+        test.expect(1);
+        var host = 'nodejs.org';
+        this.router.addhost(host);
+        test.ok(this.router.checkhost(host));
+        test.done();
+    }
+});
